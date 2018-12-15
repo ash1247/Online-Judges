5533d08f2c7ae90676038f4590c124c10734958f
ash1247 <sagor.ashikul@gmail.com>
2018-12-15 11:27:17 +0600

    Solved UVA10324

--- UVA/Medium/UVA10324.cpp
+++ UVA/Medium/UVA10324.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
 
+using namespace std;
+
+int main() {
+    //ios_base::sync_with_stdio(false);
+    //cin.ignore(0);
+    char s[1000005];
+    int cas = 0;
+    while (scanf("%s", s) != EOF ) {
+        int n, i, j, mn, mx;
+        cin >> n;
+        cas++;
+        cout << "Case " << cas << ":\n";
+        bool check;
+        while (n--) {
+            cin >> i >> j;
+            mn = min(i, j);
+            mx = max(i, j);
+            check = true;
+            for (int x = mn; x < mx; x++) {
+                if (s[x] != s[x+1]) {
+                    check = false;
+                }
+            }
+            cout << ((check) ? "Yes\n" : "No\n");
+        }
+    }
+    return 0;
+}
