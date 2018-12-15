852ff7cc9a2dce644412357153f86a594abe07ee
ash1247 <sagor.ashikul@gmail.com>
2018-12-15 09:21:06 +0600

    Solved CF 791A

--- Codeforces/600/791A.cpp
+++ Codeforces/600/791A.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int l, b;
+    int count = 1;
+    cin >> l >> b;
+    while(l < b) {
+        l *= 3;
+        b *= 2;
+        count++;
+        if ( l > b)
+            count--;
+    }
+
+    cout << count << "\n";
+    return 0;
+}
